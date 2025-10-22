f413d44341cf2dfd18e138784ed93fc6aeca7192
patricklafrance <pa.lafrance@gmail.com>
2025-10-22 18:22:08 -0400

    Refactor lint dependencies in turbo.json: removed syncpack from lint task

--- packages/eslint-configs/rslib.config.ts
+++ packages/eslint-configs/rslib.config.ts
@@ -1,7 +1,31 @@
-import { defineBuildConfig } from "@workleap/rslib-configs";
+// Standalone config because otherwise there's a dependency cycle between @workleap/eslint-configs and @workleap/rslib-configs.
+
+import { defineConfig } from "@rslib/core";
 import path from "node:path";
 
-export default defineBuildConfig({
-    target: "node",
-    tsconfigPath: path.resolve("./tsconfig.build.json")
+export default defineConfig({
+    mode: "production",
+    lib: [{
+        format: "esm",
+        syntax: "esnext",
+        bundle: false,
+        dts: true
+    }],
+    source: {
+        entry: {
+            index: path.resolve("./src/index.ts"),
+            tsconfigPath: path.resolve("./tsconfig.build.json")
+        }
+    },
+    output: {
+        target: "node",
+        distPath: {
+            root: "./dist"
+        },
+        cleanDistPath: true,
+        minify: false,
+        sourceMap: {
+            js: "source-map"
+        }
+    }
 });
