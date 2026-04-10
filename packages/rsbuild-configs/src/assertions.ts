9e5838cf86a6efe2f594464819123c5653266b7d
patrick.lafrance <pa.lafrance@gmail.com>
2026-04-10 15:06:06 -0400

    fix: adjust isFunction definition to comply with eslint rules

--- packages/rsbuild-configs/src/assertions.ts
+++ packages/rsbuild-configs/src/assertions.ts
@@ -2,8 +2,7 @@ export function isBoolean(value: unknown): value is boolean {
     return typeof value === "boolean";
 }
 
-export function isFunction(
-    value: unknown,
-): value is (...args: any[]) => unknown {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isFunction(value: unknown): value is (...args: any[]) => unknown {
     return typeof value === "function";
 }
